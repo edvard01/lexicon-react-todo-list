e46db3679501b9ee59cc5b67dd66f70b7ef919f3
edvard01 <frejaclueless@gmail.com>
2024-02-27 11:17:54 +0100

    Had to remove function from app, no need to send data to parent

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -1,3 +1,4 @@
+import { EventHandler, MouseEventHandler, useState } from "react";
 import { Navbar } from "./Navbar";
 import { TodoForm } from "./TodoForm";
 
--- todo-list/src/TodoForm.tsx
+++ todo-list/src/TodoForm.tsx
@@ -1,6 +1,10 @@
+import { MouseEventHandler, useState } from "react";
 import "./todoform.css";
 
-export function TodoForm() {
+export function TodoForm(): JSX.Element {
+  const [todo, setTodo] = useState("");
+  const [owner, setOwner] = useState("");
+
   return (
     <>
       <div className="form-container">
@@ -9,12 +13,20 @@ export function TodoForm() {
           <span>
             <label>Todo:</label>
             <br />
-            <input type="text" />
+            <input
+              type="text"
+              onChange={(e) => setTodo(e.target.value)}
+              value={todo}
+            />
           </span>
           <span>
             <label>Owner:</label>
             <br />
-            <input type="text" />
+            <input
+              type="text"
+              onChange={(e) => setOwner(e.target.value)}
+              value={owner}
+            />
           </span>
           <button type="submit">Add to list</button>
         </form>
